40f24213975d2c61826c400d81588f51ede57b7b
Dhanush Nayak <40546930+dhanushnayak@users.noreply.github.com>
2023-09-02 05:46:07 +0000

    Added Fixer

--- summarize/main.py
+++ summarize/main.py
@@ -1,14 +1,13 @@
 import os
 import click
 from transformers import pipeline
-import urllib.request
+import requests as r
 from bs4 import BeautifulSoup
 
 
 def extract_from_url(url):
     text =  ""
-    reg = urllib.request(url,data=None)
-    html  = urllib.request.urlopen(url=reg)
+    html  = r.get(url).text
     parser = BeautifulSoup(html, 'html.parser')
     for par in parser.find_all("p"):
         print(par.text)
@@ -20,8 +19,7 @@ def process(text):
     summarizer = pipeline("summarization",model='t5-small')
     result = summarizer(text,max_length=180)
     click.echo("Summarization process complete!!!")
-    click.echo("="*100)
-    click.echo(result)
+    click.secho("="*100,fg='green')
     return result[0]['summary_text']
 
 @click.command()
@@ -32,7 +30,7 @@ def main(url,file):
     elif file:
         with open(file,'r') as f:
             text = f.read()
-    click.echo(f"Summarized text -> {url or file}")
-    click.echo(process(text))
+    click.secho(f"Summarized text -> {url or file}",fg='yellow')
+    click.secho(process(text),fg='green')
 
 
